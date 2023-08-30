d102658054f389fc16bfc739faffe30a977ef5d7
Alankrita1105 <alankrita.srivastava1105@gmail.com>
2023-08-30 22:07:53 +0530

    added extras

--- JAVASCRIPT REVISION/Basic program to undestand flow of JS.js
+++ JAVASCRIPT REVISION/Basic program to undestand flow of JS.js
@@ -19,16 +19,47 @@ Hoisting in JS*/
 
 var x =7;
 function getName() {
-    console.log("Alankrita Srivastava");
+  //  console.log("Alankrita Srivastava");
 }
 getName();
 //console.log(x);
 
 /*Method 2: Hoisting in JS */
 getName1();
-console.log(y);
+//console.log(y);
+//console.log(getName1);
 
 var y=7;
 function getName1() {
-    console.log("Alankrita Srivastava");
+    //console.log("Alankrita Srivastava");
+} 
+
+var z = 7;
+function getName2() {
+ //   console.log("Hi, JS");
 }
+getName2();
+//console.log(z);
+//console.log(getName2);
+
+
+/*
+METHOD 3: FUNCTON ENVOCATION AND VARIABLE ENVIRONMENT
+*/
+
+var x = 1;
+a();
+b();
+//console.log(x);
+function a() {
+    var x = 10;
+    //console.log(x);
+}
+function b() {
+    var x = 100;
+   // console.log(x);
+}
+
+/*
+METHOD 4: UNDEFINED AND NIT DEFINED
+*/
