14d14ed4ace463e97c66ef6f549e190b77220485
Alankrita1105 <alankrita.srivastava1105@gmail.com>
2023-09-05 23:53:19 +0530

    added features

--- JAVASCRIPT REVISION/callback function.js
+++ JAVASCRIPT REVISION/callback function.js
@@ -1,3 +1,4 @@
+/*
 setTimeout( function () {
     console.log("timer");
 }, 5000);
@@ -8,4 +9,23 @@ function x(y) {
 }
 x(function y() {
     console.log("y");
-}); 
+});  
+*/
+
+function mainFunction(callback) {
+    console.log("Performing operation...");
+    // Use setTimeout to simulate an asynchronous operation
+    setTimeout(function() {
+        callback("Operation complete");
+    }, 1000);
+    }
+    
+    // Define the callback function
+    function callbackFunction(result) {
+    console.log("Result: " + result);
+    }
+    
+    // Call the main function with the callback function
+    mainFunction(callbackFunction);
+    //This code is contributed by Veerendra Singh Rajpoot
+    
