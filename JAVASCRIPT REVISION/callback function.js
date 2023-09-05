46cf34db916dd9855e821c48e85d91bf0fb5bb2a
Alankrita1105 <alankrita.srivastava1105@gmail.com>
2023-09-05 23:56:17 +0530

    added example

--- JAVASCRIPT REVISION/callback function.js
+++ JAVASCRIPT REVISION/callback function.js
@@ -12,6 +12,7 @@ x(function y() {
 });  
 */
 
+/*
 function mainFunction(callback) {
     console.log("Performing operation...");
     // Use setTimeout to simulate an asynchronous operation
@@ -28,4 +29,23 @@ function mainFunction(callback) {
     // Call the main function with the callback function
     mainFunction(callbackFunction);
     //This code is contributed by Veerendra Singh Rajpoot
-    
+*/
+
+// Define an array of numbers
+var numbers = [1, 2, 3, 4, 5];
+
+// Define the main function
+function mainFunction(callback) {
+console.log("Performing operation...");
+// Use Array.forEach to loop through the array of numbers
+numbers.forEach(callback);
+}
+
+// Define the callback function
+function callbackFunction(number) {
+console.log("Result: " + number);
+}
+
+// Call the main function with the callback function
+mainFunction(callbackFunction);
+// This code is contributed by Veerendra Singh Rajpoot
